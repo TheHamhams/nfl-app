a4382b02a8b9c652e0434a78a9ec0d76afb8b04d
Corey <hamhams86@gmail.com>
2022-06-04 14:18:17 -0700

    added formatting

--- nfl/routes.py
+++ nfl/routes.py
@@ -34,34 +34,54 @@ def home():
 def league(league_id):
     league_req = requests.get(leagues_url)
     league_data = json.loads(league_req.content)
+    division_req = requests.get(divisions_url)
+    division_data = json.loads(division_req.content)
     
     league = {}
     for leagues in league_data:
         if leagues['id'] == league_id:
             league = leagues
     
-    return render_template('league.html', data=league)
+    divisions = []
+    for division in division_data:
+        if division['league_id'] == league_id:
+            divisions.append(division)
+            
+    return render_template('league.html', data=league, divisions=divisions)
 
 @app.route('/division/<int:division_id>')
 def division(division_id):
     division_req = requests.get(divisions_url)
     division_data = json.loads(division_req.content)
+    team_req = requests.get(teams_url)
+    team_data = json.loads(team_req.content)
     
     division = {}
     for divisions in division_data:
         if divisions['id'] == division_id:
             division = divisions
             
-    return render_template('division.html', data=division)
+    teams = []
+    for team in team_data:
+        if team['division_id'] == division_id:
+            teams.append(team)
+            
+    return render_template('division.html', data=division, teams=teams)
 
 @app.route('/team/<int:team_id>')
 def team(team_id):
+    league_req = requests.get(leagues_url)
+    league_data = json.loads(league_req.content)
+    division_req = requests.get(divisions_url)
+    division_data = json.loads(division_req.content)
     team_req = requests.get(teams_url)
     team_data = json.loads(team_req.content)
     
     team = {}
     for teams in team_data:
         if teams['id'] == team_id:
-            team = teams
+            team = teams   
     
+    
+        
     return render_template('team.html', data=team)
